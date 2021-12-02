a59f46419a46bb245ec499723d0cf8d41b69f20d
Miller, Michael <MSM@taubman.com>
2021-12-01 20:53:33 -0500

    Refactor to finish challenge 2 on day 1

--- AoC2021_01_01/Depth.cs
+++ AoC2021_01_01/Depth.cs
@@ -7,28 +7,52 @@ using System.Threading.Tasks;
 namespace AoC2021_01_01;
 public class Depth
 {
-    private List<int> depths;
+    private Dictionary<int, List<int>> depths;
     public Depth()
     {
-        depths = new();
+        depths = new() { { 0, new() } };
+
     }
-    public void AddDepth(string depth)
+    public void AddSingleDepth(string input)
     {
-        if(int.TryParse(depth, out int depthInt))
-            depths.Add(depthInt);
+        int.TryParse(input, out int depthInt);
+        depths[0].Add(depthInt);
+    }
+    public void AddMultiDepth(string input)
+    {
+        int.TryParse(input, out int depthInt);
+        int maxIdx = depths.Keys.Max() + 1;
+        depths.Add(maxIdx, new() { depthInt });
+        for(int i = maxIdx - 1; i >= 0 && depths[i].Count() < 3; i--)
+        {
+            depths[i].Add(depthInt);
+        }
     }
     public int Increaments
     {
         get
         {
-            int? lastDepth = null;
             int result = 0;
-            foreach (int depth in depths)
+            int? lastDepth = null;
+            if (depths.Count == 1)
             {
-                if (lastDepth is not null && lastDepth < depth) result++;
-                lastDepth = depth;
+                foreach (int depth in depths[0])
+                {
+                    if (lastDepth is not null && lastDepth < depth) result++;
+                    lastDepth = depth;
+                }
+            }
+            else
+            {
+                foreach (var g in depths.Keys)
+                {
+                    int depth = depths[g].Sum();
+                    if (lastDepth is not null && lastDepth < depth) result++;
+                    lastDepth = depth;
+                }
             }
             return result;
+
         }
         
     }
--- AoC2021_Tests/UnitTest1.cs
+++ AoC2021_Tests/UnitTest1.cs
@@ -8,34 +8,62 @@ namespace AoC2021_Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AoC2021_01_01_Example()
         {
             Depth cut = new();
-            cut.AddDepth("199");
-            cut.AddDepth("200");
-            cut.AddDepth("208");
-            cut.AddDepth("210");
-            cut.AddDepth("200");
-            cut.AddDepth("207");
-            cut.AddDepth("240");
-            cut.AddDepth("269");
-            cut.AddDepth("260");
-            cut.AddDepth("263");
+            cut.AddSingleDepth("199");
+            cut.AddSingleDepth("200");
+            cut.AddSingleDepth("208");
+            cut.AddSingleDepth("210");
+            cut.AddSingleDepth("200");
+            cut.AddSingleDepth("207");
+            cut.AddSingleDepth("240");
+            cut.AddSingleDepth("269");
+            cut.AddSingleDepth("260");
+            cut.AddSingleDepth("263");
 
             Assert.Equal(7, cut.Increaments);
         }
+        [Fact]
+        public void AoC2021_01_02_Example()
+        {
+            Depth cut = new();
+            cut.AddMultiDepth("199");
+            cut.AddMultiDepth("200");
+            cut.AddMultiDepth("208");
+            cut.AddMultiDepth("210");
+            cut.AddMultiDepth("200");
+            cut.AddMultiDepth("207");
+            cut.AddMultiDepth("240");
+            cut.AddMultiDepth("269");
+            cut.AddMultiDepth("260");
+            cut.AddMultiDepth("263");
+
+            Assert.Equal(5, cut.Increaments);
+        }
 
         [Fact]
-        public void Test2()
+        public void AoC2021_01_01_Challenge()
         {
             Depth cut = new();
             using StreamReader sr = new StreamReader("Input_AoC2021_01_01.txt");
-            while (!sr.EndOfStream) cut.AddDepth(sr.ReadLine());
+            while (!sr.EndOfStream) cut.AddSingleDepth(sr.ReadLine());
 
             Debug.WriteLine(cut.Increaments.ToString());
 
             Assert.Equal(1711, cut.Increaments);
 
+        }
+        [Fact]
+        public void AoC2021_01_02_Challenge()
+        {
+            Depth cut = new();
+            using StreamReader sr = new StreamReader("Input_AoC2021_01_01.txt");
+            while (!sr.EndOfStream) cut.AddMultiDepth(sr.ReadLine());
+
+            Debug.WriteLine(cut.Increaments.ToString());
+
+            Assert.Equal(1743, cut.Increaments);
         }
     }
 }
