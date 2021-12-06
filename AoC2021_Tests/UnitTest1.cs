c7f3b6cc11fa679584cc15ccf62f9a6ee12948b9
Miller, Michael <MSM@taubman.com>
2021-12-06 07:36:07 -0500

    Wrong solution for Day 6.  Saving before reworking.

--- AoC2021_Tests/UnitTest1.cs
+++ AoC2021_Tests/UnitTest1.cs
@@ -94,6 +94,16 @@ namespace AoC2021_Tests
                 return new ChallengeInput(list);
             }
         }
+        public ChallengeInput AoC2021_06_SampleInput
+        {
+            get
+            {
+                List<string> list = new List<string>() {
+                    "3,4,3,1,2"
+                };
+                return new ChallengeInput(list);
+            }
+        }
         [Fact]
         public void CreateAChallengeInput()
         {
@@ -238,6 +248,30 @@ namespace AoC2021_Tests
         {
             IChallenge cut = new VentTracker(new ChallengeInput(new StreamReader("Input_AoC2021_05.txt")), ChallengeBase.Part.TWO);
             Assert.Equal(20500, cut.Answer);
+        }
+        [Fact]
+        public void AoC2021_06_01_Example()
+        {
+            IChallenge cut = new FishCounter(AoC2021_06_SampleInput, ChallengeBase.Part.ONE);
+            Assert.Equal(5934, cut.Answer);
+        }
+        [Fact]
+        public void AoC2021_06_01_Challenge()
+        {
+            IChallenge cut = new FishCounter(new ChallengeInput(new StreamReader("Input_AoC2021_06.txt")), ChallengeBase.Part.ONE);
+            Assert.Equal(363101, cut.Answer);
+        }
+        [Fact]
+        public void AoC2021_06_02_Example()
+        {
+            IChallenge cut = new FishCounter(AoC2021_06_SampleInput, ChallengeBase.Part.TWO);
+            Assert.Equal(26984457539, cut.Answer);
+        }
+        [Fact]
+        public void AoC2021_06_02_Challenge()
+        {
+            IChallenge cut = new FishCounter(new ChallengeInput(new StreamReader("Input_AoC2021_06.txt")), ChallengeBase.Part.TWO);
+            Assert.Equal(-2, cut.Answer);
         }
     }
 
---
+++ AoC_2021/FishCounter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021
+{
+    public class FishCounter : ChallengeBase
+    {
+        private readonly ChallengeInput input;
+        private readonly Part part;
+        private List<Fish> school;
+        public FishCounter(ChallengeInput input, Part part) : base(input, part)
+        {
+            this.input = input;
+            this.part = part;
+            school = new List<Fish>();
+            var initFish = input.Inputs[0].Split(",");
+            foreach(var f in initFish)
+            {
+                school.Add(new Fish(int.Parse(f)));
+            }
+        }
+        public override int Answer
+        {
+            get
+            {
+                int daysToLoop = (part == Part.ONE) ? 80 : 256;
+                
+                for(int i = 0; i < daysToLoop; i++)
+                {
+                    school.ForEach(fish => fish.ProcDay());
+                }
+                return school.Count() + school.Sum(x => x.MyFishCount);
+            }
+        }
+    }
+    internal class Fish
+    {
+        private int timer;
+        private List<Fish> childFish;
+        public Fish(int startingTimer)
+        {
+            timer = startingTimer;
+            childFish = new List<Fish>();
+        }
+        public void ProcDay()
+        {
+            childFish.ForEach(fish => fish.ProcDay());
+
+            timer--;
+            if(timer < 0)
+            {
+                timer = 6;
+                childFish.Add(new Fish(8));
+            }
+        }
+        public int MyFishCount
+        {
+            get
+            {
+                int sum = 0;
+                foreach(Fish f in childFish)
+                {
+                    sum++;
+                    sum += f.MyFishCount;
+                }
+                return sum;
+            }
+        }
+    }
+}
