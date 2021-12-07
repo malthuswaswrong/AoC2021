0797abc16b6abcd5dfb8b4e01babcaf67a51c258
Miller, Michael <MSM@taubman.com>
2021-12-06 19:49:27 -0500

    cleanup

--- AoC_2021/FishCounter.cs
+++ AoC_2021/FishCounter.cs
@@ -1,49 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AoC2021
+﻿namespace AoC2021;
+public class FishCounter : ChallengeBase
 {
-    public class FishCounter : ChallengeBase
+    private readonly ChallengeInput input;
+    private readonly Part part;
+    private long[] fishByDay = new long[9];
+    public FishCounter(ChallengeInput input, Part part) : base(input, part)
     {
-        private readonly ChallengeInput input;
-        private readonly Part part;
-        private long[] fishByDay = new long[9];
-        public FishCounter(ChallengeInput input, Part part) : base(input, part)
+        this.input = input;
+        this.part = part;
+        Array.Clear(fishByDay, 0, 9);
+        input.Inputs[0]
+            .Split(",")
+            .ToList()
+            .ForEach(f => fishByDay[int.Parse(f)]++);
+    }
+    public override long Answer
+    {
+        get
         {
-            this.input = input;
-            this.part = part;
-            Array.Clear(fishByDay, 0, 9);
-            input.Inputs[0]
-                .Split(",")
-                .ToList()
-                .ForEach(f => fishByDay[int.Parse(f)]++);
-        }
-        public override long Answer
-        {
-            get
+            int daysToLoop = (part == Part.ONE) ? 80 : 256;
+            long[] storage = new long[9];
+            for (int day = 0; day < daysToLoop; day++)
             {
-                int daysToLoop = (part == Part.ONE) ? 80 : 256;
-                long[] storage = new long[9];
-                for (int day = 0; day < daysToLoop; day++)
+                Array.Clear(storage, 0, 9);
+                for (int pointer = 8; pointer >= 0; pointer--)
                 {
-                    Array.Clear(storage, 0, 9);
-                    for (int pointer = 8; pointer >= 0; pointer--)
+                    if (pointer == 0)
                     {
-                        if (pointer == 0)
-                        {
-                            storage[8] = fishByDay[0];
-                            storage[6] += fishByDay[0];
-                        }
-                        else
-                            storage[pointer - 1] = fishByDay[pointer];
+                        storage[8] = fishByDay[0];
+                        storage[6] += fishByDay[0];
                     }
-                    storage.CopyTo(fishByDay, 0);
+                    else
+                        storage[pointer - 1] = fishByDay[pointer];
                 }
-                return fishByDay.ToList().Sum(x => x);
+                storage.CopyTo(fishByDay, 0);
             }
+            return fishByDay.ToList().Sum(x => x);
         }
     }
 }
