b742fb399fd34f8de645675c6210d17c8a7317bf
Miller, Michael <MSM@taubman.com>
2021-12-11 13:21:50 -0500

    Frivolous code condensing.

--- AoC_2021/SyntaxChecker.cs
+++ AoC_2021/SyntaxChecker.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace AoC2021;
 
-public class SyntaxChecker : ChallengeBase
-{
+public class SyntaxChecker : ChallengeBase {
     private readonly ChallengeInput input;
     private readonly Part part;
     private Dictionary<char, int> scoringTablePart1;
@@ -15,45 +14,33 @@ public class SyntaxChecker : ChallengeBase
     private readonly string openingCharacters;
     private readonly string closingCharacters;
 
-    public SyntaxChecker(ChallengeInput input, Part part) : base(input, part)
-    {
+    public SyntaxChecker(ChallengeInput input, Part part) : base(input, part) {
         this.input = input;
         this.part = part;
         openingCharacters = "([{<";
         closingCharacters = ")]}>";
-        scoringTablePart1 = new();
-        scoringTablePart1.Add(')', 3);
-        scoringTablePart1.Add(']', 57);
-        scoringTablePart1.Add('}', 1197);
-        scoringTablePart1.Add('>', 25137);
-        scoringTablePart2 = new();
-        scoringTablePart2.Add('(', 1);
-        scoringTablePart2.Add('[', 2);
-        scoringTablePart2.Add('{', 3);
-        scoringTablePart2.Add('<', 4);
+        scoringTablePart1 = new() {
+            { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 }
+        };
+        scoringTablePart2 = new() {
+            { '(', 1 }, { '[', 2 }, { '{', 3 }, { '<', 4 }
+        };
     }
 
-    public override long Answer
-    {
-        get
-        {
-
+    public override long Answer {
+        get {
             long part1Score = 0;
             List<long> part2AnswerSet = new ();
-            foreach (var line in input.Inputs)
-            {
+            foreach (var line in input.Inputs) {
                 List<char> syntaxStack = new();
                 bool lineWasValid = true;
-                foreach (char c in line)
-                {
+                foreach (char c in line) {
                     if (openingCharacters.Contains(c)) syntaxStack.Add(c);
-                    else
-                    {
+                    else {
                         if (closingCharacters.Contains(c))
                             if (syntaxStack.Last() == openingCharacters[closingCharacters.IndexOf(c)])
                                 syntaxStack.RemoveAt(syntaxStack.Count - 1);
-                            else
-                            {
+                            else {
                                 part1Score += scoringTablePart1[c];
                                 lineWasValid = false;
                                 break;
@@ -61,24 +48,16 @@ public class SyntaxChecker : ChallengeBase
                     }
                 }
 
-                if (part == Part.TWO && lineWasValid)
-                {
+                if (part == Part.TWO && lineWasValid) {
                     long lineAnswer = 0;
                     for (int i = syntaxStack.Count - 1; i >= 0; i--)
                         lineAnswer = (lineAnswer * 5) + scoringTablePart2[syntaxStack[i]];
 
-                    if (lineAnswer < 0) throw new Exception("Wut");
                     part2AnswerSet.Add(lineAnswer);
                 }
             }
-            if (part == Part.ONE)
-                return part1Score;
-            else
-            {
-                var sorted = part2AnswerSet.OrderBy(x => x).ToArray();
-                int mid = (int)Math.Floor(sorted.Length / 2d);
-                return sorted[mid];
-            }
+
+            return (part == Part.ONE) ? part1Score : part2AnswerSet.OrderBy(x => x).ToArray()[(int)Math.Floor(part2AnswerSet.Count() / 2d)];
         }
     }
 }
