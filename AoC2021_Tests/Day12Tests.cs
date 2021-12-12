3bcd685dfa731b20f9f05003e3bf33c4112b5662
Miller, Michael <MSM@taubman.com>
2021-12-12 16:29:08 -0500

    Day 12 inefficent.

---
+++ AoC2021_Tests/Day12Tests.cs
@@ -0,0 +1,102 @@
+﻿using AoC2021;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AoC2021Tests;
+
+public class Day12Tests {
+    public ChallengeInput AoC2021_12_SampleInput_Tiny1 {
+        get {
+            List<string> list = new List<string>();
+            list.Add("start-A");
+            list.Add("start-b");
+            list.Add("A-c");
+            list.Add("A-b");
+            list.Add("b-d");
+            list.Add("A-end");
+            list.Add("b-end");
+            return new ChallengeInput(list);
+        }
+    }
+    public ChallengeInput AoC2021_12_SampleInput_Tiny2 {
+        get {
+            List<string> list = new List<string>();
+            list.Add("dc-end");
+            list.Add("HN-start");
+            list.Add("start-kj");
+            list.Add("dc-start");
+            list.Add("dc-HN");
+            list.Add("LN-dc");
+            list.Add("HN-end");
+            list.Add("kj-sa");
+            list.Add("kj-HN");
+            list.Add("kj-dc");
+            return new ChallengeInput(list);
+        }
+    }
+    public ChallengeInput AoC2021_12_SampleInput {
+        get {
+            List<string> list = new List<string>() {
+                    "fs-end",
+                    "he-DX",
+                    "fs-he",
+                    "start-DX",
+                    "pj-DX",
+                    "end-zg",
+                    "zg-sl",
+                    "zg-pj",
+                    "pj-he",
+                    "RW-he",
+                    "fs-DX",
+                    "pj-RW",
+                    "zg-RW",
+                    "start-pj",
+                    "he-WI",
+                    "zg-he",
+                    "pj-fs",
+                    "start-RW"
+                };
+            return new ChallengeInput(list);
+        }
+    }
+    [Fact]
+    public void AoC2021_12_01_Example_Tiny1() {
+        IChallenge cut = new PathMapper(AoC2021_12_SampleInput_Tiny1, ChallengeBase.Part.ONE);
+        Assert.Equal(10, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_01_Example_Tiny2() {
+        IChallenge cut = new PathMapper(AoC2021_12_SampleInput_Tiny2, ChallengeBase.Part.ONE);
+        Assert.Equal(19, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_01_Example() {
+        IChallenge cut = new PathMapper(AoC2021_12_SampleInput, ChallengeBase.Part.ONE);
+        Assert.Equal(226, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_01_Challenge() {
+        IChallenge cut = new PathMapper(new ChallengeInput(new StreamReader("Input_AoC2021_12.txt")), ChallengeBase.Part.ONE);
+        Assert.Equal(3000, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_02_Example_Tiny1() {
+        IChallenge cut = new PathMapper(AoC2021_12_SampleInput_Tiny1, ChallengeBase.Part.TWO);
+        Assert.Equal(36, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_02_Example() {
+        IChallenge cut = new PathMapper(AoC2021_12_SampleInput, ChallengeBase.Part.TWO);
+        Assert.Equal(3509, cut.Answer);
+    }
+    [Fact]
+    public void AoC2021_12_02_Challenge() {
+        IChallenge cut = new PathMapper(new ChallengeInput(new StreamReader("Input_AoC2021_12.txt")), ChallengeBase.Part.TWO);
+        Assert.Equal(74222, cut.Answer);
+    }
+}
---
+++ AoC_2021/PathMapper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021;
+public class PathMapper : ChallengeBase {
+    private readonly ChallengeInput input;
+    private readonly Part part;
+    List<string> nodes;
+    List<(string p1, string p2)> paths;
+
+    public PathMapper(ChallengeInput input, Part part) : base(input, part) {
+        this.input = input;
+        this.part = part;
+        nodes = new();
+        paths = new List<(string p1, string p2)>();
+
+        this.input.Inputs.ForEach(x => {
+            var names = x.Split("-");
+            if (!nodes.Contains(names[0]))
+                nodes.Add(names[0]);
+            if (!nodes.Contains(names[1]))
+                nodes.Add(names[1]);
+            paths.Add((names[0], names[1]));
+            paths.Add((names[1], names[0]));
+        });
+    }
+    public override long Answer {
+        get {
+            var progressedRoutes = Explore("start");
+            var validRoutes = progressedRoutes.Where(x => x.EndsWith("end")).ToList();
+            return validRoutes.Count();
+        }
+    }
+    public List<string> Explore(string path) {
+        List<string> results = new List<string>();
+        string endNode = path.Split("->").Last();
+        foreach (var nextNode in paths.Where(x => x.p1 == endNode).Select(x => x.p2)) {
+            if (nextNode == "start") continue;
+            if (PathWouldBeInvalid(path, nextNode)) continue;
+            string newPath = $"{path}->{nextNode}";
+            results.Add(newPath);
+            if (nextNode != "end") {
+                results.AddRange(Explore(newPath));
+            }
+        }
+        return results;
+    }
+    private bool PathWouldBeInvalid(string path, string newNode) {
+        if (IsSmallCavern(newNode)) {
+            List<string> caves = path.Split("->").ToList();
+            caves.Add(newNode);
+            if (part == Part.ONE) {
+                return caves.Count(x => x == newNode) > 1;
+            } else {
+                Dictionary<string, int> oneCanBeTwice = new Dictionary<string, int>();
+                foreach (var cavern in caves.Where(x => IsSmallCavern(x))) {
+                    if (!oneCanBeTwice.ContainsKey(cavern))
+                        oneCanBeTwice.Add(cavern, 0);
+                    oneCanBeTwice[cavern]++;
+                }
+                if (oneCanBeTwice.Any(x => x.Value > 2))
+                    return true;
+                if (oneCanBeTwice.Count(x => x.Value > 1) > 1)
+                    return true;
+
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    bool IsSmallCavern(string name) {
+        return char.IsLower(name[0]);
+    }
+}
